781e15b98fb44763bfc79d8bf35ebeec6f545ded
MicahMeadows <micahm213@gmail.com>
2022-09-07 15:32:54 -0400

    Add sub module

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,5 @@
 const functions = require("firebase-functions");
 const api_module = require('./sender-api/app');
-// const express = require('express');
-// const cors = require('cors');
-
-// const app = express();
-// app.use(cors());
-
-
-// app.get('/test', function (req, res) {
-//   res.send('dope!');
-// });
 
 const app = api_module.app;
 
